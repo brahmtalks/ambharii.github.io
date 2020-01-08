1b4b379f30afb68237bccd697850705b2f78bf7f
kanika garg <imkanikagarg@gmail.com>
2020-01-08 20:00:42 +0530

    itssssss

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
 
   // JS tilt Effect
-function hoverTilt () {
-  var tiltBlock = $('.js-tilt');
-  if(tiltBlock.length) {
-    $('.js-tilt').tilt({
-        glare: true,
-        maxGlare: .3
-    })
-  }
-}
+// function hoverTilt () {
+//   var tiltBlock = $('.js-tilt');
+//   if(tiltBlock.length) {
+//     $('.js-tilt').tilt({
+//         glare: true,
+//         maxGlare: .3
+//     })
+//   }
+// }
+
+// ( function( $ ) {
+
+// 	"use strict";
+
+//   $(".card").tilt({
+//     maxTilt: 15,
+//     perspective: 1400,
+//     easing: "cubic-bezier(.03,.98,.52,.99)",
+//     speed: 1200,
+//     glare: true,
+//     maxGlare: 0.2,
+//     scale: 1.04
+//   });
+  
+// }( jQuery ) );
+
+// $(document).ready(function(){
+	
+// });
