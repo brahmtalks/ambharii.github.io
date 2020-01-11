4d8fbbf6fb9ae162df308423714c96cb4f5371cf
kanika garg <imkanikagarg@gmail.com>
2020-01-12 04:31:43 +0530

    aaaaa

--- main.js
+++ main.js
@@ -104,5 +104,38 @@ function myFunction2() {
 }
 // end nav bar
 
+// service section hover effect
+
+// $("img.myclass").click(function() {
+//      $("img.myclass").not(this).css("z-index", 0);
+//      $(this).css("z-index", 100);
+// });
+
+$( document ).ready(function() {
+  $("img.myclass").click(function() {
+     $("img.myclass").not(this).css("z-index", 0);
+     $(this).css("z-index", 100);
+});
+}
+
+
+
+// end of service section hover effect
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
