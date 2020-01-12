57e9b1c4d8e37771c4cbe1f722974593fdfcabc0
Aakash Pandey <Aakash.pandey@live.com>
2020-01-12 05:31:01 +0530

    code cleanup
    removed weird imports, js errors, optimised navbar

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-  // JS tilt Effect
+// JS tilt Effect
 // function hoverTilt () {
 //   var tiltBlock = $('.js-tilt');
 //   if(tiltBlock.length) {
@@ -23,11 +23,11 @@
 //     maxGlare: 0.2,
 //     scale: 1.04
 //   });
-  
+
 // }( jQuery ) );
 
 // $(document).ready(function(){
-	
+
 // });
 
 
@@ -44,53 +44,63 @@ function closeNav() {
 function openForm() {
   document.getElementById("myForm").style.display = "block";
   document.getElementsByClassName("grid")[0].style.WebkitFilter = 'blur(6px)';
-  document.getElementsByClassName("grid")[0].style.filter= 'blur(6px)';
+  document.getElementsByClassName("grid")[0].style.filter = 'blur(6px)';
 }
 
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
   document.getElementsByClassName("grid")[0].style.WebkitFilter = 'blur(0px)';
-  document.getElementsByClassName("grid")[0].style.filter= 'blur(0px)';
+  document.getElementsByClassName("grid")[0].style.filter = 'blur(0px)';
 }
 
 
-  // JS tilt Effect
-function hoverTilt () {
+// JS tilt Effect
+function hoverTilt() {
   var tiltBlock = $('.js-tilt');
-  if(tiltBlock.length) {
+  if (tiltBlock.length) {
     $('.js-tilt').tilt({
-        glare: true,
-        maxGlare: .3
+      glare: true,
+      maxGlare: .3
     })
   }
 }
 // nav bar
 
-$(document).ready(function(){
+$(document).ready(function () {
 
   $(".navbar").hide(); //Hide the navigation bar first
 
-    $(window).scroll(function () {  //Listen for the window's scroll event
-        if (isScrolledAfterElement("#content")) { //if it has scrolled beyond the #content elment
-            $('.navbar').fadeIn();  //Show the navigation bar
-        } else {
-            $('.navbar').fadeOut(); //Else hide it
-        }
-    });
-
-    //Function that returns true if the window has scrolled beyond the given element
-    function isScrolledAfterElement(elem) {
-        var $elem = $(elem);
-        var $window = $(window);
-
-        var docViewTop = $window.scrollTop();
-        var docViewBottom = docViewTop + $window.height();
-
-        var elemTop = $elem.offset().top;
-
-        return elemTop <= docViewBottom;
+  $(window).scroll(function () {  //Listen for the window's scroll event
+    if (isScrolledAfterElement("#content")) { //if it has scrolled beyond the #content elment
+      $('.navbar').fadeIn();  //Show the navigation bar
+    } else {
+      $('.navbar').fadeOut(); //Else hide it
     }
- });
+  });
+
+  $(".trigger_popup_fricc").click(function () {
+    $('.hover_bkgr_fricc').show();
+  });
+  $('.hover_bkgr_fricc').click(function () {
+    $('.hover_bkgr_fricc').hide();
+  });
+  $('.popupCloseButton').click(function () {
+    $('.hover_bkgr_fricc').hide();
+  });
+
+  //Function that returns true if the window has scrolled beyond the given element
+  function isScrolledAfterElement(elem) {
+    var $elem = $(elem);
+    var $window = $(window);
+
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+
+    return elemTop <= docViewBottom;
+  }
+});
 
 
 function myFunction1() {
@@ -104,8 +114,16 @@ function myFunction2() {
 }
 // end nav bar
 
-
-
+// window.onscroll = function() {myFunction()};   
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;   
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
 
 
 
