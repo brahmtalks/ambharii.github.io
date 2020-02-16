864ac5593e3bbddad9d6671fae85e2bebead9b1d
kanika garg <imkanikagarg@gmail.com>
2020-02-17 01:42:46 +0530

    vehle vehle

--- emb/main.js
+++ emb/main.js
@@ -1,11 +1,11 @@
 
-// function openNav() {
-//   document.getElementById("drawer").style.width = "550px";
-// }
+function openNav() {
+  document.getElementById("drawer").style.width = "550px";
+}
 
-// function closeNav() {
-//   document.getElementById("drawer").style.width = "0";
-// }
+function closeNav() {
+  document.getElementById("drawer").style.width = "0";
+}
 
 function myFunction1() {
   var elmnt = document.getElementById("features");
@@ -16,3 +16,24 @@ function myFunction2() {
   var elmnt = document.getElementById("services");
   elmnt.scrollIntoView();
 }
+
+
+$('.slickk').slick({
+  accessibility: false,
+  infinite: true,
+  slidesToShow: 1,
+  autoplay: true,
+  arrows: false,
+  dots: false,
+  swipeToSlide: true,
+  infinite: false
+});
+
+$('.slickkk').slick({
+  accessibility: false,
+  infinite: true,
+  slidesToShow: 4,
+  autoplay: true,
+  arrows: true,
+  swipeToSlide: true
+});
